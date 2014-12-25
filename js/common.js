a840345ac7c3a51366003d130a4874e63343d067
splintex <eugene.godun@gmail.com>
2014-12-25 17:09:19 +0530

    upd index

--- js/common.js
+++ js/common.js
@@ -28,45 +28,46 @@ $(document).ready(function() {
 	$(".js-nav-all").text(lengthSections);
 
 	var fpage = $('.js-fullpage');
-	fpage.fullpage({
-		anchors: ['section1', 'section2', 'section3', 'section4', 'section5', 'section6'],
-		// sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
-		//navigation: true,
-		//navigationPosition: 'right',
-		//navigationTooltips: ['First page', 'Second page', 'Third and last page'],
-		sectionSelector: '.js-section',
-		controlArrows: true,
-		verticalCentered: true,
-		fixedElements: '.header, .footer',
-		responsive: 1,
-		easing: 'easeInQuart',
-		easingcss3: 'ease-in-out',
-		onLeave: function(index, nextIndex, direction){
-			$(".js-nav-curent").text(nextIndex);
-		},
-		afterLoad: function(anchorLink, index){
-            //using index
-            if(index == 1){
-                $(".js-nav-prev").addClass("is-inactive");
-            }
-            else {
-            	$(".js-nav-prev").removeClass("is-inactive");
-            }
-            if(index == lengthSections){
-                $(".js-nav-next").addClass("is-inactive");
-            }
-            else {
-            	$(".js-nav-next").removeClass("is-inactive");
-            }
+	if (fpage.length) {
+		fpage.fullpage({
+			anchors: ['section1', 'section2', 'section3', 'section4', 'section5', 'section6'],
+			// sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+			//navigation: true,
+			//navigationPosition: 'right',
+			//navigationTooltips: ['First page', 'Second page', 'Third and last page'],
+			sectionSelector: '.js-section',
+			controlArrows: true,
+			verticalCentered: true,
+			fixedElements: '.header, .footer',
+			responsive: 1,
+			easing: 'easeInQuart',
+			easingcss3: 'ease-in-out',
+			onLeave: function(index, nextIndex, direction){
+				$(".js-nav-curent").text(nextIndex);
+			},
+			afterLoad: function(anchorLink, index){
+	            //using index
+	            if(index == 1){
+	                $(".js-nav-prev").addClass("is-inactive");
+	            }
+	            else {
+	            	$(".js-nav-prev").removeClass("is-inactive");
+	            }
+	            if(index == lengthSections){
+	                $(".js-nav-next").addClass("is-inactive");
+	            }
+	            else {
+	            	$(".js-nav-next").removeClass("is-inactive");
+	            }
 
-            // //using anchorLink
-            // if(anchorLink == 'secondSlide'){
-            //     alert("Section 2 ended loading");
-            // }
-        }
-	});
+	            // //using anchorLink
+	            // if(anchorLink == 'secondSlide'){
+	            //     alert("Section 2 ended loading");
+	            // }
+	        }
+		});
 
-	
+	}
 
 	function navBtn() {
 		var index = $(".js-section.active").index();
